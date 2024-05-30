c2897a27254e47cf5d61b28e6177e5fed71e5de9
AlexGyver <alexgyvershow@ya.ru>
2024-05-30 13:49:33 +0300

    Update mqtt.h

--- src/esp/sync/mqtt.h
+++ src/esp/sync/mqtt.h
@@ -21,8 +21,15 @@ class HubMQTT : public gh::Bridge {
 
    public:
     // настроить MQTT (хост брокера, порт, логин, пароль, QoS, retained)
-    void config(const String& host, uint16_t port, const String& login = "", const String& pass = "", uint8_t qos = 0, bool ret = 0) {
-        if (!host.length()) return;
+    void config(const char* host, uint16_t port, const String& login = "", const String& pass = "", uint8_t qos = 0, bool ret = 0) {
+        if (!host) return;
+        mq_host = host;
+        mqtt.setServer(mq_host.c_str(), port);
+        _config(login, pass, qos, ret);
+    }
+
+    void config(const __FlashStringHelper* host, uint16_t port, const String& login = "", const String& pass = "", uint8_t qos = 0, bool ret = 0) {
+        if (!host) return;
         mq_host = host;
         mqtt.setServer(mq_host.c_str(), port);
         _config(login, pass, qos, ret);
